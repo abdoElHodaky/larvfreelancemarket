5783f4f94a92dfbcd34b234ca6ab7fe3384ea3e4
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-22 12:22:08 +0300

    Update footer.blade.php

--- resources/views/components/app-layouts/footer.blade.php
+++ resources/views/components/app-layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer mt-auto py-3 bg-light position-fixed w-100" style="bottom: 0">
   <div class="container">
-    <span class="text-muted">Freelancers Market | <a href="https://github.com/abdoelhodaky/larvfreelancemarket">source code on <i class="fab fa-github"></i></a></span>
+    <span class="text-muted">Freelancers Market | <a href="#">source code on <i class="fab fa-github"></i></a></span>
   </div>
 </footer>
